bacee60c3764bd18b41b05b93837c30bd7fdd7cc
claudiacazacu <claudia-mihaela.cazacu@s.unibuc.ro>
2025-11-02 19:53:10 +0200

    m1 v3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <limits>
 #include <string>
 #include <fstream>
-//#include <Helper.h>
+#include <algorithm>
 
 class Articol {
     std::string Nume;
@@ -13,9 +13,8 @@ class Articol {
     std::string Culoare;
     int Pret;
 
-
-    public:
-    Articol(const std::string& nume, const std::string& tip, const std::string& culoare,int pret)
+public:
+    Articol(const std::string& nume, const std::string& tip, const std::string& culoare, int pret)
         : Nume(nume), Tip(tip), Culoare(culoare), Pret(pret) {}
 
     void GetInfo() const {
@@ -23,16 +22,15 @@ class Articol {
     }
 
     const std::string& GetNume() const { return Nume; }
+    const std::string& GetTip() const { return Tip; }
     int GetPret() const { return Pret; }
 
-
     friend std::ostream& operator<<(std::ostream& os, const Articol& articol){
-        os<<articol.Nume<<" ";
-        os<<articol.Tip<<" ";
-        os<<articol.Culoare<<" ";
-        os<<articol.Pret<<"\n";
+        os << articol.Nume << " ";
+        os << articol.Tip << " ";
+        os << articol.Culoare << " ";
+        os << articol.Pret;
         return os;
-
     }
 };
 
@@ -42,7 +40,7 @@ class Dulap{
     int Capacitate;
     std::string Proprietar;
 
-    std::list<Articol> ArticoleInceput(){
+    std::list<Articol> ArticoleInceput() const {
         return {
             Articol("Tricou1", "Vara","Alb", 0),
             Articol("Pantaloni1","Vara","Gri", 0),
@@ -53,9 +51,17 @@ class Dulap{
         };
     }
 
-    public:
+public:
     Dulap(int capacitate, const std::string& proprietar)
     :Articole(ArticoleInceput()), Capacitate(capacitate), Proprietar(proprietar){}
+
+    int CalculeazaValoareTotala() const {
+        int valoare = 0;
+        for (const auto& articol : Articole) {
+            valoare += articol.GetPret();
+        }
+        return valoare;
+    }
 
     bool EstePlin() const {
         return (int)Articole.size() >= Capacitate;
@@ -72,14 +78,19 @@ class Dulap{
             articol.GetInfo();
         }
     }
+
+    const std::list<Articol>& GetArticole() const { return Articole; }
+
+
     friend std::ostream& operator<<(std::ostream& os, const Dulap& dulap){
-        os<<"Dulapul lui: " << dulap.Proprietar << "\n"
-            << "Capacitatea de : " << dulap.Capacitate << "\n"
-            << "Articolele: \n";
+        os << "Dulapul lui: " << dulap.Proprietar << "\n"
+           << "Capacitatea de : " << dulap.Capacitate << "\n"
+           << "Valoare totala: " << dulap.CalculeazaValoareTotala() << "\n"
+           << "Articolele: \n";
         for (const auto& articol : dulap.Articole) {
-        os << articol << "\n"; 
-    }
-    return os;
+            os << articol << "\n";
+        }
+        return os;
     }
 };
 
@@ -90,64 +101,73 @@ class Personaj {
     Dulap dulap;
     int Sold;
 
-    public:
+public:
     Personaj(const std::string& nume, const std::string& nationalitate, int nivel, int sold=100)
         : Nume(nume), Nationalitate(nationalitate), Nivel(nivel), dulap(15, nume), Sold(sold){}
 
-    void GetInfo() const {
-        std::cout << "Nume: " << Nume << ", Nationalitate: " << Nationalitate << ", Nivel: " << Nivel << "\n";
-        dulap.GetInfo();
-    }
-    const Dulap& GetDulap() const {
-        return dulap;
-    }
-
-    bool Cumparaturi(const Articol& articol){
-    if (Sold < articol.GetPret()) {
-        return false;
-    }
-    if (dulap.EstePlin()) {
-        return false;
-    }
-    dulap.AdaugaArticol(articol);
-    Sold -= articol.GetPret();
-    return true;
-}
-
-    int GetSold() const { return Sold; }
-
-
-    //constructorul meu de copiere
-    //in contextul jocului meu, vreau ca personajul sa aiba versiunea
-    //cu outfit de vara si outfit de iarna
     Personaj( const Personaj &v2)
     : Nume(v2.Nume), Nationalitate(v2.Nationalitate), Nivel(v2.Nivel), dulap(v2.dulap), Sold(v2.Sold) {}
 
-    //operatorul de copiere
-    //vreau sa pot da reset la outfitul default
     Personaj& operator= (const Personaj &altul){
         if( this != &altul) {
             Nume = altul.Nume;
             Nationalitate = altul.Nationalitate;
             Nivel = altul.Nivel;
-            dulap=altul.dulap;
+            dulap = altul.dulap;
             Sold = altul.Sold;
-            //nu stiu inca ce adaug aici 
         }
         return *this;
-    };
+    }
 
     ~Personaj() {
         std::cout << "Personajul " << Nume << " a fost distrus.\n";
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const Personaj& personaj){
-        os<<"Numele: "<<personaj.Nume <<"\n"
-            <<"Nationalitatea: "<<personaj.Nationalitate<<"\n"  
-            <<"Nivelul: "<<personaj.Nivel<<"\n";
-        return os;
+    void GetInfo() const {
+        std::cout << "Nume: " << Nume << ", Nationalitate: " << Nationalitate << ", Nivel: " << Nivel << ", Sold: " << Sold << "\n";
+        dulap.GetInfo();
     }
 
+    const Dulap& GetDulap() const {
+        return dulap;
+    }
+
+    bool Cumparaturi(const Articol& articol){
+        if (Sold < articol.GetPret()) {
+            return false;
+        }
+        if (dulap.EstePlin()) {
+            return false;
+        }
+        dulap.AdaugaArticol(articol);
+        Sold -= articol.GetPret();
+        return true;
+    }
+
+    int GetSold() const { return Sold; }
+
+    void AfiseazaEchipamentDeSezon(const std::string& sezon) const {
+        std::cout << "\nEchipament " << sezon << " al lui " << Nume << ":\n";
+        bool gasit = false;
+        for (const auto& articol : dulap.GetArticole()) {
+            if (articol.GetTip() == sezon) {
+                std::cout << "  ";
+                articol.GetInfo();
+                gasit = true;
+            }
+        }
+        if (!gasit) {
+            std::cout << "Nu exista articole de sezonul " << sezon << " in dulap.\n";
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Personaj& personaj){
+        os << "Numele: "<< personaj.Nume << "\n"
+           << "Nationalitatea: "<< personaj.Nationalitate << "\n"  
+           << "Nivelul: "<< personaj.Nivel << "\n"
+           << "Sold: "<< personaj.Sold << "\n";
+        return os;
+    }
 };
 
 
@@ -155,17 +175,17 @@ class Animal{
     std::string Nume;
     std::string Specie; 
 
-    public:
-    
+public:
     Animal(const std::string& nume, const std::string& specie)
         : Nume(nume), Specie(specie) {}
 
     void GetInfo() const {
         std::cout << "Nume: " << Nume << ", Specie: " << Specie << "\n";
     }
+
     friend std::ostream& operator<<(std::ostream& os, const Animal& animal){
-        os<<"Animalutul este "<<animal.Specie
-            <<" cu numele "<<animal.Nume<<"\n";
+        os << "Animalutul este "<< animal.Specie
+           << " cu numele "<< animal.Nume << "\n";
         return os;
     }
 };
@@ -175,51 +195,21 @@ class Magazin{
     std::list<Articol> Articole;
     std::string Nume;
 
-    std::list<Articol> Articolele(){
+    std::list<Articol> Articolele() const {
         return{
-            //top vara
-            Articol("Tricou1", "Vara", "Negru", 10),
-            Articol("Tricou2", "Vara", "Gri", 10),
-            Articol("Tricou3", "Vara", "Albastru", 10),
-            Articol("Tricou4", "Vara", "Roz", 10),
-        
-            //pantaloni vara
-            Articol("Pantaloni1", "Vara", "Negru", 20),
-            Articol("Pantaloni2", "Vara", "Gri", 20),
-            Articol("Pantaloni3", "Vara", "Albastru", 20),
-            Articol("Pantaloni4", "Vara", "Roz", 20),
-
-            //top iarna
-            Articol("Hanorac1", "Iarna", "Negru", 25),
-            Articol("Hanorac2", "Iarna", "Roz", 25),
-
-            //pantaloni iarna
-            Articol("Pantaloni5", "Iarna", "Alb", 30),
-            Articol("Pantaloni6", "Iarna", "Roz", 30),
-            Articol("Pantaloni7", "Iarna", "Gri", 30),
-
-            //pantofi
-            Articol("Adidasi2", "Vara", "Gri", 40),
-            Articol("Adidasi3", "Vara", "Negru", 40),
-            Articol("Sandale", "Vara", "Roz", 35),
-            Articol("Bocanci2", "Iarna", "Alb", 50),
-            Articol("Cizme", "Iarna", "Gri", 55),
-
-            //accesorii
-            Articol("Ochelari de soare", "Vara", "Negru", 15),
-            Articol("Sapca", "Vara", "Albastru", 10),
-            Articol("Fular", "Iarna", "Rosu", 15),
-            Articol("Caciula", "Iarna", "Negru", 12)
-
+            Articol("Tricou1_mag", "Vara", "Negru", 10),
+            Articol("Tricou2_mag", "Vara", "Gri", 10),
+            Articol("Pantaloni1_mag", "Vara", "Negru", 20),
+            Articol("Hanorac1_mag", "Iarna", "Negru", 25),
+            Articol("Pantaloni5_mag", "Iarna", "Alb", 30),
+            Articol("Adidasi2_mag", "Vara", "Gri", 40),
+            Articol("Ochelari de soare", "Vara", "Negru", 15)
         };
-
     }
 
-    public:
+public:
     explicit Magazin(const std::string& nume)
-    : Articole(Articolele()), Nume(nume) {
-        this->Nume=nume;
-    }
+    : Articole(Articolele()), Nume(nume) {}
 
     void GetInfo() const {
         std::cout << "Magazin: " << Nume << "\n";
@@ -231,10 +221,18 @@ class Magazin{
 
     const std::list<Articol>& GetArticole() const { return Articole; }
 
+    const Articol* CautaArticol(const std::string& numeArticol) const {
+        for (const Articol& articol : Articole) {
+            if (articol.GetNume() == numeArticol) {
+                return &articol;
+            }
+        }
+        return nullptr;
+    }
 
     friend std::ostream& operator<<(std::ostream& os, const Magazin& magazin){
-        os<<"Magazinul: " << magazin.Nume << "\n"
-            << "Articolele disponibile: \n";
+        os << "Magazinul: " << magazin.Nume << "\n"
+           << "Articolele disponibile: \n";
         for (const auto& articol : magazin.Articole) {
             os << articol << "\n"; 
         }
@@ -248,25 +246,7 @@ int main() {
         std::cerr << "N-am putut deschide tastatura.txt\n";
         return 1;
     }
-    //char rasp;
 
-    /*while(true){
-        std::cout << "START JOC?! (Y/N) : ";
-        std::cin >> rasp ;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        
-        if(rasp=='Y' || rasp=='y'){
-            break;
-        }
-        else if(rasp=='N' || rasp=='n'){
-            std::cout << "Oprim jocul.!\n";
-            return 0;
-        }
-        else{
-            std::cout << "Raspuns invalid. Te rog sa alegi Y sau N.\n"; 
-        }
-    }
-        */
     std::string nume;
     std::vector<std::string> nationalitati = {"Romanian", "French", "Spanish", "Italian"};
 
@@ -275,59 +255,78 @@ int main() {
 
     std::cout << "Nationalitatile:\n";
     for(size_t i=0; i < nationalitati.size(); i++){
-        std::cout << i+1 << "; " << nationalitati[i]<<"\n";
+        std::cout << i+1 << "; " << nationalitati[i] << "\n";
     }
 
     int optiune;
     while(true){
-        std::cout<< "Alege nationalitatea: ";
-        fin >> optiune;
+        std::cout << "Alege nationalitatea: ";
+        if (!(fin >> optiune)) { 
+            std::cerr << "Eroare la citire sau EOF.\n";
+            return 1;
+        }
         if (optiune > 0 && optiune <= (int)nationalitati.size()) break;
         std:: cout << "Alegere invalida. Haide sa intelegem din nou!\n";
     }
 
-   Personaj player(nume, nationalitati[optiune - 1], 1, 100);
+    Personaj player(nume, nationalitati[optiune - 1], 1, 100);
+    Personaj player_default = player;
 
-   std::cout << "Dulapul initial al personajului:\n" << player.GetDulap() << "\n";
+    std::cout << "\nStarea initiala a Personajului:\n" << player;
+    std::cout << "Dulapul initial:\n" << player.GetDulap() << "\n";
+    
+    std::cout << "Detalii Personaj:\n";
+    player.GetInfo();
 
+    player.AfiseazaEchipamentDeSezon("Vara");
+    
     std::string rasp2;
-    fin>>rasp2;
+    fin >> rasp2;
     if (rasp2 == "Y" || rasp2 == "y") {
         Magazin magazin("Magazinul Jocului");
-        std::cout<< magazin << "\n";
 
-        std::cout <<"Ai "<<player.GetSold()<<"bani \n";
+        std::cout << "\nInformatii Magazin:\n";
+        magazin.GetInfo();
 
-        std::cout<<"Ce vrei sa cumperi? *introduci numele aici\n";
+        std::cout << "\nArticole Magazin:\n" << magazin << "\n";
+
+        std::cout << "Ai " << player.GetSold() << " bani \n";
+        std::cout << "Ce vrei sa cumperi? *introduci numele aici\n";
+        
         std::string raspArticol;
-        fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Clear the newline character from the input buffer
+        fin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         std::getline(fin, raspArticol);
 
-        const Articol* articolGasit = nullptr;
-        for (const Articol& articol : magazin.GetArticole()) {
-            if (articol.GetNume() == raspArticol) {
-                articolGasit = &articol;
-                break;
+        const Articol* articolGasit = magazin.CautaArticol(raspArticol);
+        
+        if (articolGasit) {
+            std::cout << "Valoarea dulapului inainte de cumparaturi: " << player.GetDulap().CalculeazaValoareTotala() << "\n";
+            
+            if (player.Cumparaturi(*articolGasit)){
+                std::cout << "Ai cumparat: " << articolGasit->GetNume() << ".\n";
+                std::cout << "Soldul tau este acum: " << player.GetSold() << "\n";
+            } else {
+                std::cout << "Nu s-a putut cumpara. Nu ai suficienti bani sau dulapul este plin.\n";
             }
-        }
-    if(articolGasit) {
-        if(player.Cumparaturi(*articolGasit)){
-            std::cout << "Ai cumparat: " << articolGasit->GetNume() << ". Soldul tau este acum: " << player.GetSold() << "\n";
         } else {
-            std::cout << "Nu ai suficienti bani sau dulapul este plin.\n";
+            std::cout << "Articolul **" << raspArticol << "** nu a fost gasit in magazin.\n";
         }
-        } else {
-        std::cout << "Articolul nu a fost gasit in magazin.\n";
-        }
-        std::cout<<"Dulapul acum:\n"<< player.GetDulap() << "\n";
-
-        return 0;
+        std::cout << "\nDulapul dupa cumparaturi:\n" << player.GetDulap() << "\n";
     }
-   
-    //Personaj personajul1("Anelise", "French", 10);
-    //Personaj personajul2("Claudia", "Romanian", 5);
+
+    std::cout << "\nTestare Constructor de Copiere:\n";
+    Personaj player_iarna(player);
+    std::cout << "Noul personaj (copie): " << player_iarna;
     
-    //personajul1.GetInfo();
-    //personajul2.GetInfo();
+    std::cout << "\nTestare Operator= de Copiere (Reset):\n";
+    player = player_default;
+    std::cout << "Personajul principal a fost resetat:\n" << player.GetDulap() << "\n";
+
+    std::cout << "\nTestare Clasa Animal:\n";
+    Animal caine("Rex", "Caine");
+    Animal pisica("Miau", "Pisica");
+    caine.GetInfo(); 
+    std::cout << pisica; 
+
     return 0;
 }
